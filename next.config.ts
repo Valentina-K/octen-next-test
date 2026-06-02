f75d68fb2ee6deb81b7363b376168fc36dc77260
Valentina-K <kvalentyna@hotmail.com>
2026-06-02 14:46:38 +0300

    delete gh-pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    basePath: "/octen-next-test",
     images:{
         remotePatterns: [{
             protocol: 'https',
