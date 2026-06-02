7651b283371051b500471fb7a6248f59e307afc5
Valentina-K <kvalentyna@hotmail.com>
2026-06-02 14:08:26 +0300

    add gh-pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    basePath: "/octen-next-test",
     images:{
         remotePatterns: [{
             protocol: 'https',
